342de4dc566df47ea6dc2b2164f1e520095b810a
NorhanFoda <norhan.h.foda@gmail.com>
2019-12-03 13:43:19 +0200

    3-12-2019-FirstCommit_3

---
+++ database/migrations/2019_12_01_125441_create_AstMarket_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateAstMarketTable extends Migration {
+
+	public function up()
+	{
+		Schema::create('AstMarket', function(Blueprint $table) {
+			$table->increments('ID_No');
+			$table->timestamps();
+			$table->integer('Mrkt_No')->nullable();    //رقم المشرف
+			$table->integer('Brn_No')->nullable();      //رقم الفرع
+			$table->string('Mrkt_NmEn')->nullable();     //الاسم
+			$table->string('Mrkt_NmAr')->nullable();
+		});
+	}
+
+	public function down()
+	{
+		Schema::drop('AstMarket');
+	}
+}
