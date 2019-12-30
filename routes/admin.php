96a60e94f6e7d0955ab0e028ffac5e689fb6ff77
mai-adel88 <maiadel799@gmail.com>
2019-12-30 13:15:53 +0200

    mai30

--- routes/admin.php
+++ routes/admin.php
@@ -254,7 +254,9 @@ Route::group(['prefix'=>'admin'],function (){
 
         //Notices
         Route::resource('notice', 'Admin\Notice\NoticeController');
-
+        //Route::post('notice/debit', 'Admin\Notice\NoticeController@getDebitPage')->name('getDebit');
+        Route::get('getcr', 'Admin\Notice\NoticeController@getSelect')->name('getSelect');
+        Route::get('getdb', 'Admin\Notice\NoticeController@getDbSelect')->name('getDbSelect');
         Route::get('hijriNoti', 'Admin\Notice\NoticeController@convertToDateToHijri')->name('hijriNoti');
 
         Route::post('getSalesManN', 'Admin\Notice\NoticeController@getSalesMan')->name('getSalesManN');
